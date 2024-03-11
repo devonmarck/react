4b2d6bc8b201b73b175f293b2f995a9e0834964f
OTAVIO LUIZ MARTINS DUARTE <otavio.lmduarte@senacsp.edu.br>
2024-03-11 12:03:35 -0300

    criaçao do  home

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,37 @@
 import './App.css'
 import Descricao from './components/Descricao/Descricao';
- 
-import Header from './components/Header/header';
+ import {craete BrowserRouter, RouterProvider} from 'react-router-dom;'
+import Home from './components/Home/Home';
+
+
+
+ import Header from './components/Header/header';
 import Main from './components/Main/Main'
+import { createBrowserRouter } from 'react-router-dom';
  
 function App() {
  
+const router = createBrowserRouter([
+
+{
+  path: "/",//rota da url
+  element: <Home/> //componente a ser exibido
+},
+
+{
+  path: "/desc-sonicExe",//rota da url
+  element: <Descricao/> //componente a ser exibido
+
+},
+{ 
+path: "/desc-sonicExe2",//rota da url
+element: <Descricao2/> //componente a ser exibido
+}
+
+])}
+
+
+
   return (
    <div>
       <Header />
